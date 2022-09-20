cb606ee0064bd82fb467fa350171e011c64aab68
John Mathews <johnmatthews4@wayfair.com>
2022-09-20 16:54:11 +0200

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     es6: true,
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended", "next", "next/core-web-vitals"],
-  // extends: ['eslint:recommended', 'next', 'next/core-web-vitals'],
   rules: {
     "prettier/prettier": "error",
     "react/react-in-jsx-scope": "off",
