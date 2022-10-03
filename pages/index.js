148369b82c60a95541307f5a587818310f37c3df
John Mathews <john.mathews@partner.air-up.com>
2022-10-03 19:08:02 +0200

    remove landing page title and site description

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,13 @@ export default function Home({ posts }) {
 
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Home
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
+          <Image
+            src="https://picsum.photos/seed/123/1000/800"
+            alt="placeholder"
+            width="1000"
+            height="800"
+          />
         </div>
-
-        <Image
-          src="https://picsum.photos/seed/123/1000/800"
-          alt="placeholder"
-          width="1000"
-          height="800"
-        />
 
         <ul className="">
           {!posts.length && "No posts found."}
