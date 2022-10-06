e8ef55997335a64f8c6778dc893b9cdff097eaf5
John Mathews <john.mathews@partner.air-up.com>
2022-10-06 10:03:39 +0200

    keyboard shortcuts

--- components/KeyboardShortcuts.js
+++ components/KeyboardShortcuts.js
@@ -6,6 +6,29 @@ const KeyboardShortcuts = () => {
   const router = useRouter()
   // const navigate = useNavigate();
   // const goToSnippets = useCallback(() => navigate('/snippets', { replace: true }), [navigate]);
+
+  useMousetrap("j", () => {
+    window.scrollBy({ top: 200, left: 0, behavior: "smooth" })
+  })
+
+  useMousetrap("k", () => {
+    window.scrollBy({ top: -200, left: 0, behavior: "smooth" })
+  })
+
+  useMousetrap("g f", () => {
+    window.history.forward()
+  })
+  useMousetrap("g b", () => {
+    window.history.back()
+  })
+
+  // useMousetrap('g g', () => {
+  //   $('html, body').animate({ scrollTop: 0 }, 500)
+  // })
+  // useMousetrap('G', () => {
+  //   $('html, body').animate({ scrollBottom: 0 }, 500)
+  // })
+
   useMousetrap("g s", () => {
     router.push("/snippets")
   })
