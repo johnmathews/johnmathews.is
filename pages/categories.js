e52fa606c17e9528477adfff03eb9d8a5a55b064
John Mathews <john.mathews@partner.air-up.com>
2022-10-28 18:21:14 +0200

    more cats

--- pages/categories.js
+++ pages/categories.js
@@ -27,33 +27,63 @@ export default function Categories({ categories }) {
   })
   console.log("--- debug structuredCategories: ", structuredCategories)
 
-  const sortedCategories = Object.keys(categories).sort((a, b) => categories[b] - categories[a])
+  // const sortedCategories = Object.keys(categories).sort((a, b) => categories[b] - categories[a])
+
+  //         <ul>
+  //           {sortedCategories.map((t) => {
+  //             return (
+  //               <li key={t} className="mt-2 mb-2 mr-5 text-lg">
+  //                 <Category text={t} />
+  //                 <Link
+  //                   href={`/categories/${kebabCase(t)}`}
+  //                   className="ml-2 font-semibold uppercase text-gray-600 dark:text-gray-300"
+  //                 >
+  //                   {` (${categories[t].length})`}
+  //                 </Link>
+  //               </li>
+  //             )
+  //           })}
+  //         </ul>
   return (
     <>
       <PageSEO title={`Categories - ${siteMetadata.author}`} description="Things I blog about" />
       <div className="md:mt-24">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div id="titleWrapper" className="space-x-2 pt-6 pb-8 md:space-y-5">
+          <div
+            id="title"
+            className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+          >
             Categories
-          </h1>
+          </div>
         </div>
-        <div className="flex max-w-lg flex-wrap">
-          {Object.keys(categories).length === 0 && "No categories found."}
-          <ul>
-            {sortedCategories.map((t) => {
-              return (
-                <li key={t} className="mt-2 mb-2 mr-5 text-lg">
-                  <Category text={t} />
-                  <Link
-                    href={`/categories/${kebabCase(t)}`}
-                    className="ml-2 font-semibold uppercase text-gray-600 dark:text-gray-300"
-                  >
-                    {` (${categories[t].length})`}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
+        <div id="categories" className="flex max-w-lg flex-col">
+          {Object.keys(structuredCategories).map((category) => {
+            return (
+              <div
+                key={category}
+                className="my-3 text-2xl font-bold capitalize text-gray-900 dark:text-gray-100 "
+              >
+                {category}
+
+                {structuredCategories[category].forEach((subCat) => {
+                  console.log("--- debug subCat: ", subCat)
+                  Object.keys(subCat).map((subCatKey) => {
+                    console.log("--- debug subCat key: ", subCatKey)
+                    console.log("--- debug subCat posts: ", subCat[subCatKey])
+                    return (
+                      <div
+                        id="subCat"
+                        key={subCatKey}
+                        className="my-3 text-lg font-bold capitalize text-gray-900 dark:text-gray-100 "
+                      >
+                        {subCatKey}
+                      </div>
+                    )
+                  })
+                })}
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
