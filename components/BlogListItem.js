7b47641b1924917ba0d593fc3bb4bad45a1e5662
John Mathews <johnmatthews4@wayfair.com>
2022-11-06 23:59:07 +0100

    grouping by year

--- components/BlogListItem.js
+++ components/BlogListItem.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const BlogListItem = ({ post }) => {
   console.log("--- debug post.title: ", post.title)
-  return <div key={post.slug}>post.title</div>
+  return <div>post.title</div>
 }
 
 export default BlogListItem
