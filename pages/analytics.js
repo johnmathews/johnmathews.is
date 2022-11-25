628512c6a14804ae0ac82405f168f11802422b09
John Mathews <john.mathews@air-up.com>
2022-11-25 22:48:38 +0100

    all working

--- pages/analytics.js
+++ pages/analytics.js
@@ -14,29 +14,27 @@ export default function Analytics() {
   const ASSETS_LOCATION = "assets.johnmathews.is"
 
   const daily_costs = `https://${ASSETS_LOCATION}/website-analytics/daily_costs.JSON`
-  const client_events = `https://${ASSETS_LOCATION}/website-analytics/aggregate-client-js-events.JSON`
-  const page_views_ip_addresses = `https://${ASSETS_LOCATION}/website-analytics/aggregate-website-analyics.JSON`
-
   const {
     isLoading: dailyCostsIsLoading,
     data: dailyCostsData,
     error: dailyCostsError,
   } = useFetch(daily_costs)
+  console.log("--- debug dailyCostsData: ", dailyCostsData)
 
-  const {
-    isLoading: clientEventsIsLoading,
-    data: clientEventsData,
-    error: clientEventsError,
-  } = useFetch(client_events)
-
+  const page_views_ip_addresses = `https://${ASSETS_LOCATION}/website-analytics/aggregate-website-analyics.JSON`
   const {
     isLoading: pageViewsIsLoading,
     data: pageViewsData,
     error: pageViewsError,
   } = useFetch(page_views_ip_addresses)
-
-  console.log("--- debug dailyCostsData: ", dailyCostsData)
   console.log("--- debug pageViewsData: ", pageViewsData)
+
+  const client_events = `https://${ASSETS_LOCATION}/website-analytics/aggregate-client-js-events.JSON`
+  const {
+    isLoading: clientEventsIsLoading,
+    data: clientEventsData,
+    error: clientEventsError,
+  } = useFetch(client_events)
   console.log("--- debug clientEventsData: ", clientEventsData)
 
   return (
@@ -51,23 +49,21 @@ export default function Analytics() {
         )}
         {dailyCostsData && <div>{dailyCostsData["daily_costs"]} </div>}
       </div>
-
       <div className="my-3">
         <div className="text-2xl font-semibold">Page Views</div>
         {pageViewsIsLoading && <div>A moment please...</div>}
         {pageViewsError && (
           <div>{`There is a problem fetching the page views data - ${pageViewsError}`}</div>
         )}
-        {pageViewsData && <div>{pageViewsData} </div>}
+        {pageViewsData && <div>{pageViewsData["daily_views"]} </div>}
       </div>
-
       <div className="my-3">
         <div className="text-2xl font-semibold">Client Events</div>
         {clientEventsIsLoading && <div>A moment please...</div>}
         {clientEventsError && (
           <div>{`There is a problem fetching the client events data - ${clientEventsError}`}</div>
         )}
-        {clientEventsData && <div>{clientEventsData} </div>}
+        {clientEventsData && <div>{clientEventsData.daily_events_by_type} </div>}
       </div>
     </>
   )
