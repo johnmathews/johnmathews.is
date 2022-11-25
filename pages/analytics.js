395eb2e996554bbac35c29719a15db7263de7a76
John Mathews <john.mathews@air-up.com>
2022-11-25 17:34:11 +0100

    this works

--- pages/analytics.js
+++ pages/analytics.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react"
-import useFetch from "react-fetch-hook"
 
 import PageTitle from "@/components/PageTitle"
 
@@ -8,41 +7,48 @@ export async function getStaticProps() {
 }
 
 // https://blog.logrocket.com/modern-api-data-fetching-methods-react/
-// https://recharts.org/en-US/guide/getting-started
+// https://recharts.org/en-US/
 
 export default function Analytics() {
   const ASSETS_LOCATION = "assets.johnmathews.is"
-
   const daily_costs = `https://${ASSETS_LOCATION}/website-analytics/daily_costs.JSON`
-  const client_events = `https://${ASSETS_LOCATION}/website-analytics/aggregate-client-js-events.JSON`
-  const page_views_ip_addresses = `https://${ASSETS_LOCATION}/website-analytics/aggregate-website-analyics.JSON`
+  console.log("--- debug daily_costs: ", daily_costs)
+  // const client_events = `https://${ASSETS_LOCATION}/website-analytics/aggregate-client-js-events.JSON`
+  // const page_views_ip_addresses = `https://${ASSETS_LOCATION}/website-analytics/aggregate-website-analyics.JSON`
 
-  const { costsIsLoading, costsData, costsError } = useFetch(daily_costs)
-  const { clientEventsIsLoading, clientEventsData, clientEventsError } = useFetch(client_events)
-  const { pageViewsIsLoading, pageViewsData, pageViewsError } = useFetch(page_views_ip_addresses)
+  const [costData, setCostData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-  console.log("--- debug costsError: ", costsError)
-  console.log("--- debug costsIsLoading: ", costsIsLoading)
-  console.log("--- debug costsData: ", costsData)
+  useEffect(() => {
+    fetch(daily_costs)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`This is an HTTP error: The status is ${response.status}`)
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setCostData(data["daily_costs"])
+        setError(null)
+      })
+      .catch((err) => {
+        console.log(err.message)
+        setError(err.message)
+        setCostData(null)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [daily_costs])
   return (
     <>
       <PageTitle>{"Analytics"}</PageTitle>
-      {costsIsLoading && <div>A moment please...</div>}
-      {costsError && (
-        <div>{`There is a problem fetching the daily costs data - ${costsError}`}</div>
-      )}
-      {costsData}
-      {clientEventsIsLoading && <div>A moment please...</div>}
-      {clientEventsError && (
-        <div>{`There is a problem fetching the client eventsdata - ${clientEventsError}`}</div>
-      )}
-      {clientEventsData}
-      {pageViewsIsLoading && <div>A moment please...</div>}
-      {pageViewsError && (
-        <div>{`There is a problem fetching the page views data - ${pageViewsError}`}</div>
-      )}
-      {pageViewsData}
-      foo
+
+      {loading && <div>A moment please...</div>}
+      {error && <div>{`There is a problem fetching the data - ${error}`}</div>}
+
+      {costData}
     </>
   )
 }
