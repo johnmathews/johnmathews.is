14fa58bafeaeac26faf3ed9707f7e10e28c92eaa
John Mathews <john.mathews@air-up.com>
2022-12-14 23:39:23 +0100

    add tooltip

--- components/homeBrewAnalytics/locationCountItem.js
+++ components/homeBrewAnalytics/locationCountItem.js
@@ -8,7 +8,11 @@ export default function LocationCountItem({ value }) {
   const key = `${value[0]}_${getRandomInt()}`
   const srcUrl = `https://flagcdn.com/20x15/${value[0].toLowerCase()}.png`
   return (
-    <div key={key} className="mx-1 flex h-8 rounded-lg border-2 border-gray-300 px-2">
+    <div
+      key={key}
+      title={value[0]}
+      className="mx-1 flex h-8 rounded-lg border-2 border-gray-300 px-2"
+    >
       <Image width={25} height={12} alt={value[0]} src={srcUrl} />
       <div>{value[1]}</div>
     </div>
