15d51b58db4330dcdfd44e8a3ecc92212a1833c0
John Mathews <john.mathews@air-up.com>
2022-12-15 09:37:59 +0100

    flags

--- components/homeBrewAnalytics/locationCountItem.js
+++ components/homeBrewAnalytics/locationCountItem.js
@@ -7,14 +7,18 @@ function getRandomInt() {
 export default function LocationCountItem({ value }) {
   const key = `${value[0]}_${getRandomInt()}`
   const srcUrl = `https://flagcdn.com/20x15/${value[0].toLowerCase()}.png`
+  // const srcUrl = "/static/images/landing/test-image.jpeg"
+
   return (
     <div
       key={key}
       title={value[0]}
-      className="mx-1 my-2 flex rounded-lg border-2 border-gray-300 px-2"
+      className="mx-1 flex h-8 rounded-lg border-2 border-gray-300 py-1 px-1"
     >
-      <Image className="mr-2" width="20" height="15" alt={value[0]} src={srcUrl} />
-      <div>{value[1]}</div>
+      <div className="mr-1 h-7">
+        <Image className="leading-none" width="20" height="15" alt={value[0]} src={srcUrl} />
+      </div>
+      <div className="pt-1 leading-none">{value[1]}</div>
     </div>
   )
 }
