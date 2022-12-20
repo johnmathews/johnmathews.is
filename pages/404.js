46d70a31e40e7dc5b39bd58b54068ba112df9f16
John Mathews <john.mathews@air-up.com>
2022-12-20 11:22:25 +0100

    404

--- pages/404.js
+++ pages/404.js
@@ -6,9 +6,9 @@ export default function FourZeroFour() {
   return (
     <>
       <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
-      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+      <div className="flex flex-col items-start justify-start md:mb-10 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-12">
+        <div className="space-x-2 pb-8 md:space-y-5">
+          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-12 md:text-8xl md:leading-14">
             404
           </h1>
         </div>
@@ -16,12 +16,15 @@ export default function FourZeroFour() {
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
             Sorry we couldn't find this page.
           </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
+          <p className="my-3">
+            If you're looking for a page that used to exist, or have followed an old link, its
+            probably moved.
           </p>
-          <Link href="/">
-            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-              Back to homepage
+          <p>Try putting `/blog/` before the page name. </p>
+          <p className="my-3"> Or, try searching for it (cmd-k).</p>
+          <Link className="" href="/posts">
+            <button className="focus:shadow-outline-blue my-10 inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+              Back to blog
             </button>
           </Link>
         </div>
