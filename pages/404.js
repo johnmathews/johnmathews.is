6b1c7d635f7e3d097ca6f897cdf120fb46cc4d7a
John Mathews <john.mathews@air-up.com>
2022-12-29 11:29:48 +0100

    404 page

--- pages/404.js
+++ pages/404.js
@@ -16,12 +16,19 @@ export default function FourZeroFour() {
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
             Sorry we couldn't find this page.
           </p>
-          <p className="my-3">
+          <p className="my-2">
             If you're looking for a page that used to exist, or have followed an old link, its
             probably moved.
           </p>
-          <p>Try putting `/blog/` before the page name. </p>
-          <p className="my-3"> Or, try searching for it (cmd-k).</p>
+          <p className="my-3"> Try searching for it.</p>
+          <span className="my-2 flex">
+            Search:{" "}
+            <pre className="ml-2 rounded-md border bg-slate-300 px-2 dark:bg-gray-600">cmd - k</pre>
+          </span>
+          <span className="my-2 flex">
+            Keyboard shortcuts:{" "}
+            <pre className="ml-2 rounded-md border bg-slate-300 px-2 dark:bg-gray-600">?</pre>
+          </span>
           <Link className="" href="/posts">
             <button className="focus:shadow-outline-blue my-10 inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
               Back to blog
