e2a6fc6b972dff67f07ee6c339805020e5e9d68d
John Mathews <john.mathews@air-up.com>
2023-01-09 15:17:36 +0100

    more collections

--- pages/collectionTemplate.js
+++ pages/collectionTemplate.js
@@ -1,30 +1,29 @@
 import siteMetadata from "@/data/siteMetadata"
-import bibleData from "@/data/bibleNotesPageData"
+import data from "@/data/bibleNotesPageData"
 import Link from "@/components/Link"
 import { PageSEO } from "@/components/SEO"
 
 export default function BookNotes() {
   return (
     <>
-      <PageSEO title={`Bible - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO title={`template - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Bible Notes
+            Template
           </h1>
-          <p>These are quick and candid thoughts on various parts of the bible.</p>
-          <p> They're not particularly well considered, and the opinions are lightly held.</p>
+          <p>sub title</p>
         </div>
         <div className="container py-12">
           <div className="-m-4">
-            {Object.keys(bibleData).map((h) => {
+            {Object.keys(data).map((h) => {
               return (
                 <div key={h} className="ml-5">
                   <h2 className="-mb-2 text-lg font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10 md:text-2xl md:leading-14">
                     {h}
                   </h2>
                   <ul className="ml-5 list-disc ">
-                    {bibleData[h].map((d) => (
+                    {data[h].map((d) => (
                       <li key={d} className="my-3">
                         <Link
                           href={d.href}
