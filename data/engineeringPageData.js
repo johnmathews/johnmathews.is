80a7e3d0a5cd7a0b09022f380369e0d279fa7e78
John Mathews <john.mathews@air-up.com>
2023-01-09 15:33:55 +0100

    collections

--- data/engineeringPageData.js
+++ data/engineeringPageData.js
@@ -1,28 +1,10 @@
 const metaPageData = {
   General: [
     {
-      title: "A list of unconnected thoughts and aphorisms",
-      description: "",
-      imgSrc: "",
-      href: "/blog/a-list-of-unconnected-thoughts-and-aphorisms",
-    },
-    {
-      title: "How to get rich without getting lucky",
-      description: "Notes on a twtter thread by @Naval",
-      imgSrc: "",
-      href: "/blog/how-to-get-rich-without-getting-lucky",
-    },
-    {
       title: "Skills needed by a senior engineer",
       description: "",
       imgSrc: "",
       href: "/blog/an-incomplete-list-of-skills-senior-engineers-need",
-    },
-    {
-      title: "Questions for interesting conversations",
-      description: "A list of questions that might lead to interesting or intense conversations.",
-      imgSrc: "",
-      href: "/blog/questions",
     },
     {
       title: "Startup engineering lessons",
