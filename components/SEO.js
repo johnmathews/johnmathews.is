320124955579aca215b2be7c487e998c371e4039
John Mathews <john.mathews@air-up.com>
2023-01-16 16:50:09 +0100

    add default description

--- components/SEO.js
+++ components/SEO.js
@@ -184,7 +184,7 @@ export const BlogSEO = ({
         url: `${siteMetadata.siteUrl}${siteMetadata.siteLogo}`,
       },
     },
-    description: summary || description,
+    description: summary || description || siteMetadata.defaultPostDescription,
   }
 
   const twImageUrl = featuredImages[0].url
