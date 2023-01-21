fb10bacc5249fa386f8e4d8653eeabf4d326783b
John Mathews <john.mathews@air-up.com>
2023-01-21 22:09:32 +0100

    one file is tsx

---
+++ components/Image.tsx
@@ -0,0 +1,10 @@
+import NextImage from "next/image"
+import React from "react"
+
+type ImageProps = React.ComponentProps<typeof NextImage>
+
+function Image({ ...rest }: ImageProps) {
+  return <NextImage {...rest} />
+}
+
+export default Image
