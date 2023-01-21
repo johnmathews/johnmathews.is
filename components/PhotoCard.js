ec88dd2ba62c2c1bbe848dff07e953a1e7367097
John Mathews <john.mathews@air-up.com>
2023-01-21 21:37:28 +0100

    make sure site still builds

--- components/PhotoCard.js
+++ components/PhotoCard.js
@@ -6,7 +6,7 @@ const Card = ({ title, imgSrc, href }) => (
     <div
       className={`${
         imgSrc && "h-full"
-      }  overflow-hidden rounded-md border-2 border-gray-700 border-opacity-60 bg-slate-200 dark:border-gray-400  dark:border-gray-700 dark:bg-slate-700 `}
+      }  overflow-hidden rounded-md border-2 border-gray-700 border-opacity-60 bg-slate-200 dark:border-gray-400  dark:bg-slate-700 `}
     >
       {imgSrc &&
         (href ? (
