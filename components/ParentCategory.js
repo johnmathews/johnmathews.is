8e64160a3636ac5e8ba4f9bb26a1c8777c770ac8
John Mathews <john.mathews@air-up.com>
2023-02-23 11:20:53 +0100

    title-case hyphenated category group correctly

--- components/ParentCategory.js
+++ components/ParentCategory.js
@@ -3,23 +3,27 @@ import kebabCase from "@/lib/utils/kebabCase"
 import ChildCategory from "@/components/ChildCategory"
 
 const ParentCategory = ({ catName, structuredCategories }) => {
-  if (catName != "snippet") {
-    return (
-      <div key={catName}>
-        <div className="my-3 text-2xl font-bold capitalize text-gray-900 dark:text-gray-100 ">
-          {catName}
-        </div>
-
-        <div>
-          {structuredCategories.map((category) => {
-            return <ChildCategory key={category} parentName={catName} category={category} />
-          })}
-        </div>
-      </div>
-    )
+  var category = ""
+  // this is because the `capitalize` className doesnt work with hyphens
+  if (catName == "non-technical") {
+    category = "Non-Technical"
   } else {
-    return <div key={catName}></div>
+    category = catName
   }
+
+  return (
+    <div key={catName}>
+      <div className="my-3 text-2xl font-bold capitalize text-gray-900 dark:text-gray-100 ">
+        {category}
+      </div>
+
+      <div>
+        {structuredCategories.map((category) => {
+          return <ChildCategory key={category} parentName={catName} category={category} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default ParentCategory
