f10fec9bd30fb974893d3bdc272a503ade3722ca
John Mathews <john.mathews@air-up.com>
2023-03-07 12:31:35 +0100

    prettier formatting single quotes

--- data/summariesData.js
+++ data/summariesData.js
@@ -1,27 +1,27 @@
 const sectionsData = [
   {
-    title: "April 2021: Learning",
-    description: "GCP Service Accounts, SSH, Ansible, Vagrant",
-    imgSrc: "",
-    href: "/blog/learning-april-2021",
+    title: 'April 2021: Learning',
+    description: 'GCP Service Accounts, SSH, Ansible, Vagrant',
+    imgSrc: '',
+    href: '/blog/learning-april-2021',
   },
   {
-    title: "April 2021: Reading",
-    description: "1-Person SaaS, Lots of technical stuff.",
-    imgSrc: "",
-    href: "/blog/reading-april-2021",
+    title: 'April 2021: Reading',
+    description: '1-Person SaaS, Lots of technical stuff.',
+    imgSrc: '',
+    href: '/blog/reading-april-2021',
   },
   {
-    title: "April 2021: Tweets",
-    description: "Surpringly interesting tweets",
-    imgSrc: "",
-    href: "/blog/tweets-april-2021",
+    title: 'April 2021: Tweets',
+    description: 'Surpringly interesting tweets',
+    imgSrc: '',
+    href: '/blog/tweets-april-2021',
   },
   {
-    title: "April 2018: Reading",
+    title: 'April 2018: Reading',
     description: "Investing, Mental Models, lots of 'meta' things",
-    imgSrc: "",
-    href: "/blog/reading-april-2018",
+    imgSrc: '',
+    href: '/blog/reading-april-2018',
   },
 ]
 
--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   semi: false,
-  singleQuote: false,
+  singleQuote: true,
   jsxSingleQuote: false,
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   bracketSpacing: true,
-  endOfLine: "auto",
+  endOfLine: 'auto',
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
