0f46b11357ad48871535ef3a1675efbc6ba6f8b9
John Mathews <john.mathews@air-up.com>
2023-03-07 13:03:10 +0100

    single quotes and formatting

--- pages/bible.js
+++ pages/bible.js
@@ -1,10 +1,10 @@
-import siteMetadata from "@/data/siteMetadata"
-import bibleData from "@/data/bibleNotesPageData"
-import Link from "@/components/Link"
-import { PageSEO } from "@/components/SEO"
+import siteMetadata from '@/data/siteMetadata'
+import bibleData from '@/data/bibleNotesPageData'
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
 
-import { AppContext } from "@/components/ContextProvider"
-import { useContext } from "react"
+import { AppContext } from '@/components/ContextProvider'
+import { useContext } from 'react'
 
 export default function BookNotes() {
   const [state, _] = useContext(AppContext)
@@ -12,12 +12,12 @@ export default function BookNotes() {
     <>
       <PageSEO title={`Bible - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5   ">
+          <h1 className="text-3xl font-extrabold leading-9  tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Bible Notes
           </h1>
           <p>These are quick and candid thoughts on various parts of the bible.</p>
-          <p> They're not particularly well considered, and the opinions are lightly held.</p>
+          <p>They're not particularly well considered, and the opinions are lightly held.</p>
         </div>
         <div className="container py-12">
           <div className="-m-4">
