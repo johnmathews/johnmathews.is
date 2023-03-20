25b751077438e66a0cb37a322be9ae1649c416cc
John Mathews <john.mathews@air-up.com>
2023-03-20 11:09:24 +0100

    add description attribute to photograpgs collection

--- data/photographsPageData.ts
+++ data/photographsPageData.ts
@@ -5,21 +5,25 @@ const photographsPageData: CollectionItem[] = [
     title: 'Beach Photos',
     imgSrc: '/static/images/beach-photos/wind.jpeg',
     href: '/blog/beach-photographs',
+    description: 'Photos from a trip to the beach.',
   },
   {
     title: 'Mist',
     imgSrc: '/static/images/photos/path_to_knowwhere.jpeg',
     href: '/blog/spaarndam-photographs',
+    description: '',
   },
   {
     title: 'Spaarndam',
     imgSrc: '/static/images/photos/dog_walker.jpeg',
     href: '/blog/photographs',
+    description: '',
   },
   {
     title: 'Live near the ocean',
     imgSrc: '/static/images/california2.jpg',
     href: '/blog/ocean',
+    description: '',
   },
 ]
 
