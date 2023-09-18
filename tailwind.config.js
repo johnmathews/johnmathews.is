6bd7438bd71344185890ec552e127331cb5ee46d
John Mathews <mthwsjc@gmail.com>
2023-09-18 14:00:16 +0200

    tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,22 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js, ts, jsx, tsx}',
+  ],
   experimental: {
     optimizeUniversalDefaults: true,
   },
-  content: [
-    './pages/**/*.{js, ts, jsx, tsx}',
-    './components/**/*.{js, ts, jsx, tsx}',
-    // './app/**/*.{js,ts,jsx,tsx}',
-    // './layouts/**/*.{js, ts, jsx, tsx}',
-    // './lib/**/*.{js, ts, jsx, tsx}',
-    // './data/**/*.{mdx, md}',
-  ],
+  // content: [
+  //   './pages/**/*.{js, ts, jsx, tsx}',
+  //   './components/**/*.{js, ts, jsx, tsx}',
+  //   './app/**/*.{js,ts,jsx,tsx}',
+  //   './layouts/**/*.{js, ts, jsx, tsx}',
+  //   './lib/**/*.{js, ts, jsx, tsx}',
+  //   './data/**/*.{mdx, md}',
+  // ],
   darkMode: 'class',
   theme: {
     screens: {
