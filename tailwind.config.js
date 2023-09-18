6b9a90112465143d5186b4a7d2c67bd025c25b70
John Mathews <mthwsjc@gmail.com>
2023-09-18 14:50:53 +0200

    remove mode argument too

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   // purge: [
   //   './pages/**/*.{js,ts,jsx,tsx}',
   //   './components/**/*.{js,ts,jsx,tsx}',
