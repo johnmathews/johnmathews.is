585a72b0229797420f426ac5fa25a77139d29153
John Mathews <mthwsjc@gmail.com>
2023-09-20 11:41:46 +0200

    change referrer policy

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
+    value: 'origin-when-cross-origin',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
