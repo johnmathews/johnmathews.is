527c16e64facd0a8a7966a25cfeae0a0fbaebf8f
John Mathews <mthwsjc@gmail.com>
2023-09-20 12:17:21 +0200

    add api key env var collection correctly for vercel deployment

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self' johnmathews.is  api.openai.com ojfdsjhuemowizxtijbk.supabase.co ;
+  default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app platform.twitter.com ;
-  style-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com ;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com ;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
