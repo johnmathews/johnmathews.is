0d9bfea5967841d3643ba8bc0e9394371fc411e1
John Mathews <mthwsjc@gmail.com>
2023-09-22 11:56:46 +0200

    revert change

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -29,7 +29,7 @@ const handler = async (req: Request): Promise<Response> => {
     const json = await res.json()
     const embedding = json.data[0].embedding
 
-    const { data: chunks, error } = await supabaseAdmin.rpc('blog_content', {
+    const { data: chunks, error } = await supabaseAdmin.rpc('blog_search', {
       query_embedding: embedding,
       similarity_threshold: 0.01,
       match_count: matches,
