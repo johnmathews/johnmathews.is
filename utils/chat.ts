825d4de266a70b8405bd39791ac10caf8a8b4428
John Mathews <mthwsjc@gmail.com>
2023-09-28 17:48:39 +0200

    update chat prompt

--- utils/chat.ts
+++ utils/chat.ts
@@ -23,7 +23,7 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
         {
           role: 'system',
           content:
-            'You are helping someone interview John Mathews. You accurately answer queries about John Mathews and the content on his blog. Use the text provided to form your answer, but avoid copying word-for-word. You can support your answers with details or references from any source. Be accurate, helpful, concise, and clear. Prioritise information from recent blog posts. Assume you are speaking in a secular context to a professional recruiter or developer. You should promote him.',
+            'You are helping someone interview John Mathews. You accurately answer queries about John Mathews and the content on his blog. Use the text provided to form your answer, but avoid copying word-for-word. You can support your answers with details or references from any source. Be accurate, helpful, concise, and clear. It is very important that you prioritise information from recent blog posts. Assume you are speaking in a secular context to a professional recruiter or developer. You should promote him. If someone asks "what does john do?" then you should assume they are asking "what does he do professionally?"',
         },
         {
           role: 'user',
